0844bb4dba8361fad07cdaff470feb1d2ff6f112
Nicholas Kubas <nicholaskubas@Nicholass-MacBook-Air.local>
2018-09-21 12:52:03 -0700

    I created a weight_earth variable, then inserted the expressions representing the new weights into the string to be printed.

--- planets.py
+++ planets.py
@@ -1,7 +1,9 @@
 def weight_on_planets():
    # write your code here
+   weight_earth = int(input('What do you weigh on Earth? '))
    
-   
+   print(f'\nOn Mars you would weigh {weight_earth*.38} pounds.' + \
+         f'\nOn Jupiter you would weigh {weight_earth*2.34} pounds.')  
    
 if __name__ == '__main__':
    weight_on_planets()
